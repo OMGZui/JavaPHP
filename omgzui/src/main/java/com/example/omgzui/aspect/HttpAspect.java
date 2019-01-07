6153e79d0fbcf753f503d9c2adac90a8902e539b
小粽子 <862275679@qq.com>
2019-01-07 14:48:12 +0800

    切片AOP

---
+++ omgzui/src/main/java/com/example/omgzui/aspect/HttpAspect.java
@@ -0,0 +1,54 @@
+package com.example.omgzui.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class HttpAspect {
+
+    public static final Logger logger = LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * com.example.omgzui.controller.GirlController.*(..))")
+    public void log() {
+
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        //url
+        logger.info("url={}", request.getRequestURL());
+
+        //method
+        logger.info("method={}", request.getMethod());
+
+        //ip
+        logger.info("ip={}", request.getRemoteAddr());
+
+        //类方法
+        logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+
+        //参数
+        logger.info("args={}", joinPoint.getArgs());
+    }
+
+    @After("log()")
+    public void doAfter() {
+        logger.info("22222222222222");
+    }
+
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturning(Object object) {
+        logger.info("response={}", object.toString());
+    }
+}
